62e6dd526a7bd800aa86f8f2983cf35e6287ac94
Sushma-kurva <shusmashusma69@gmail.com>
2025-02-26 20:20:04 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,89 @@
+#date 13_02_2025
+
+'''
+Kalam was elected as the president of India in 2002 with the support of both the ruling Bharatiya Janata Party 
+
+and the then-opposition Indian National Congress. He was widely referred to as the "People's President". 
+He engaged in teaching, writing and public service after his presidency. 
+He was a recipient of several awards, including the Bharat Ratna, India's highest civilian honour.
+'''
+# fullname=input("enter the fullname:")
+# presidentyear=input("enter the year:")
+# print(f"full name is {fullname}\n presindentyear is {presidentyear}")
+
+'''
+Telugu cinema, also known as Tollywood, is the segment of Indian cinema dedicated
+ to the production of motion pictures in the Telugu language,
+ widely spoken in the states of Andhra Pradesh and Telangana. Based in Film Nagar,
+ Hyderabad, Telugu cinema is the largest film industry in India by box-office revenue as of 2021
+# '''
+# telugucinema=input("enter the telugucinema:")
+# spokenstate=input("enter the spokenstate:")
+# print(f"telugu cinema is {telugucinema}\n spoken state is {spokenstate}")
+
+# colors={
+#     "males_fav_colors":["blue","green","white","gray"],
+#     "females_fav_colors":["pink","orange","sky-blue","black"]
+# }
+# pick_up_colors=input("enter your color:")
+# if pick_up_colors in colors["males_fav_colors"]:
+#     print(f"hey your pick_up_color {pick_up_colors} is in males_fav_colors")
+# elif pick_up_colors in colors["females_fav_colors"]:
+#     print(f"hey your pink_up_color {pick_up_colors} is in females_fav_colors")
+# else:
+#     print(f"sorry your pick_up_color {pick_up_colors} is not available")
+
+# department=input("Enter your department:")
+# if department=="cse" or department=="it":
+#     print(f" hi {department} guys plz come inside")
+# elif department=="teacher":
+#     print(f"sorry mam/sir plz go to 2nd gate")
+# elif department=="chief_guest":
+#     print("sorry sir plz go to 3rd gate")
+# else:
+#     print("sorry not allowed")
+
+# numbers=[1,2,2,3,3,3,4,4,4,4,4,5,5,5,5,5,]
+# count_number={}
+# for i in numbers:
+#     if i in count_number:
+#         count_number[i]+=1
+#     else:
+#         count_number[i]=1
+#         print(count_number)
+# #range(start,end,step)
+# for i in range (1,10,2):
+#     print(i)
+# name="sushma"
+# if name=="siri":
+#     print(" your name is siri")
+# elif name=="sushma":
+#     print(" your name is sushma")
+# elif  name=="akhil":
+#     print(" your name is akhil")
+# else:
+#     print("name isn't sushma,swathi or akhil")
+x=10
+y=5
+# if x>5:
+#     if y>5:
+#         print("x and y are greater than 5")
+#     elif y==5:
+#         print("x is greater than 5 and y is 5")
+#     else:
+#         print("x is greater than 5 and y is less than 5")
+office_name="microsoft"
+if office_name=="wipro":
+    print(" your office_name is a wipro")
+elif office_name=="tcs":
+    print(" your office_name is a tcs")
+elif office_name=="infosys":
+    print("your office_name is a infosys")
+elif office_name=="amazon":
+    print("your office_name is a amazon")
+elif office_name=="cognizant":
+    print("your office_name is a cognizant")
+elif office_name=="microsoft":
+    print("your office_name is a microsoft")
+else:
+    print("office_name isn't wipro,tcs,infosys,amazon, or cognizant")
